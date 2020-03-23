1023d26ea733123a67c95cf25e70ed06b99c12fe
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-03-23 08:46:02 -0500

    working on form requests

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="email"/>
+        <input name="password" placeholder="password"/>
+        <input name="passwordConfirmation" placeholder="confirm password"/>
+        <button>Sign Up</button>
+      </form>
+    </div>
+  `);
+});
+
+app.post("/", (req, res) => {
+  // get acces to email, password, passwordConfirmation
+  req.on("data", data => {
+    console.log(data);
+  });
+  res.send("Account Created!");
 });
 
 app.listen(3000, () => {

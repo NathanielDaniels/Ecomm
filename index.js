951b638745969eec5fe8296f8b7858670d6823c3
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-03-26 13:57:11 -0500

    added bodyparser

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-// const nodemon = require("nodemon");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+// This helps with Dry Code Practices
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
 
 app.get("/", (req, res) => {
   res.send(`
@@ -16,29 +21,32 @@ app.get("/", (req, res) => {
   `);
 });
 
-const bodyParser = (req, res, next) => {
-  if (req.method === "POST") {
-    req.on("data", data => {
-      const parsed = data.toString("utf8").split("&");
-      const formData = {};
-      for (let pair of parsed) {
-        const [key, value] = pair.split("=");
-        formData[key] = value;
-      }
-      console.log(formData);
-      req.body = formData;
-    });
-  } else {
-    next();
-  }
-  // get acces to email, password, passwordConfirmation
-};
-
 app.post("/", (req, res) => {
-  // console.log(req);
-  res.send("Account Created!");
-});
-
+  console.log(req.body);
+  res.send("Account Created!")
 app.listen(3000, () => {
   console.log("listening");
 });
+
+//==========================================================================
+//! BodyParser by hand
+//* This is now replaced by require('body-parser') + bodyParser.urlencoded({ extended: true })
+
+// const bodyParser = (req, res, next) => {
+//   if (req.method === "POST") {
+//     // get acces to email, password, passwordConfirmation
+//     req.on("data", data => {
+//       const parsed = data.toString("utf8").split("&");
+//       const formData = {};
+//       for (let pair of parsed) {
+//         const [key, value] = pair.split("=");
+//         formData[key] = value;
+//       }
+//       // console.log(formData);
+//       req.body = formData;
+//       next();
+//     });
+//   } else {
+//     next();
+//   }
+// };

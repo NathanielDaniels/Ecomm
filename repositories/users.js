0faf5b3fd1f07c3d4dee7e855997c0d266602c7c
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-04-26 17:15:36 -0500

    updated users.json with new password info. Working Update function Complete

--- repositories/users.js
+++ repositories/users.js
@@ -66,9 +66,10 @@ class UsersRepository {
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
+  console.log(await repo.getAll());
   // await repo.delete("89d2f99f");
   // await repo.create({ email: "Nathan@gmail.com", password: "password" });
-  await repo.update("9812f08e", { password: "myNEWpassword" });
+  await repo.update("9812f08e", { password: "NewpPassword" });
 };
 
 test();

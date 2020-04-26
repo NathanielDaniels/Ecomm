727eb3a65dc8dbc638802a49807dd1c80c09dc08
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-04-25 19:08:24 -0500

    understanding update( ) before moving on

--- repositories/users.js
+++ repositories/users.js
@@ -66,10 +66,9 @@ class UsersRepository {
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
-
   // await repo.delete("89d2f99f");
   // await repo.create({ email: "Nathan@gmail.com", password: "password" });
-  await repo.update("89d2f99f", { password: "mypassword" });
+  await repo.update("9812f08e", { password: "myNEWpassword" });
 };
 
 test();

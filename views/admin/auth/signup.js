1906e412a48050170a698f70de1303a74a4a3892
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-05-03 03:32:09 -0500

    creating a layout file instead for dry code

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,15 +1,14 @@
 module.exports = ({ req }) => {
   return `
-  
-      <div>
-        Your ID is: ${req.session.userId} <hr>
-        <form method="POST" style="width: 50%; margin: 10px;" >
-          <input name="email" placeholder="email"/>
-          <input name="password" placeholder="password" style="margin: 5px 0;"/>
-          <input name="passwordConfirmation" placeholder="confirm password"/>
-          <br>
-          <button style="margin: 5px 0;">Sign Up</button>
-        </form>
-      </div>
+    <div>
+      Your ID is: ${req.session.userId} <hr>
+      <form method="POST" style="width: 50%; margin: 10px;" >
+        <input name="email" placeholder="email"/>
+        <input name="password" placeholder="password" style="margin: 5px 0;"/>
+        <input name="passwordConfirmation" placeholder="confirm password"/>
+        <br>
+        <button style="margin: 5px 0;">Sign Up</button>
+      </form>
+    </div>
   `;
 };

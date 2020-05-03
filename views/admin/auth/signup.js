4ad685a3c6d6f5260700c07801f46b2a770fbfdb
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-05-03 03:28:26 -0500

    added full html doctype to each file

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,14 +1,20 @@
 module.exports = ({ req }) => {
   return `
-    <div>
-      Your ID is: ${req.session.userId} <hr>
-      <form method="POST" style="width: 50%; margin: 10px;" >
-        <input name="email" placeholder="email"/>
-        <input name="password" placeholder="password" style="margin: 5px 0;"/>
-        <input name="passwordConfirmation" placeholder="confirm password"/>
-        <br>
-        <button style="margin: 5px 0;">Sign Up</button>
-      </form>
-    </div>
+  <!DOCTYPE html>
+  <html lang='eng'>
+    <head>
+    </head>
+    <body>
+      <div>
+        Your ID is: ${req.session.userId} <hr>
+        <form method="POST" style="width: 50%; margin: 10px;" >
+          <input name="email" placeholder="email"/>
+          <input name="password" placeholder="password" style="margin: 5px 0;"/>
+          <input name="passwordConfirmation" placeholder="confirm password"/>
+          <br>
+          <button style="margin: 5px 0;">Sign Up</button>
+        </form>
+      </div>
+    </body>
   `;
 };

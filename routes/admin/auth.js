2e3efb210a44b5020843d83ddd35037f2db908d1
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-05-03 03:05:36 -0500

    added signup + signin.js and adding html from auth.js

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -4,16 +4,7 @@ const router = express.Router();
 
 router.get("/signup", (req, res) => {
   res.send(`
-    <div>
-      Your ID is: ${req.session.userId} <hr>
-      <form method="POST" style="width: 50%; margin: 10px;" >
-        <input name="email" placeholder="email"/>
-        <input name="password" placeholder="password" style="margin: 5px 0;"/>
-        <input name="passwordConfirmation" placeholder="confirm password"/>
-        <br>
-        <button style="margin: 5px 0;">Sign Up</button>
-      </form>
-    </div>
+    
   `);
 });
 
---
+++ views/admin/auth/signup.js
@@ -0,0 +1,14 @@
+module.exports = () => {
+  return `
+    <div>
+      Your ID is: ${req.session.userId} <hr>
+      <form method="POST" style="width: 50%; margin: 10px;" >
+        <input name="email" placeholder="email"/>
+        <input name="password" placeholder="password" style="margin: 5px 0;"/>
+        <input name="passwordConfirmation" placeholder="confirm password"/>
+        <br>
+        <button style="margin: 5px 0;">Sign Up</button>
+      </form>
+    </div>
+  `;
+};

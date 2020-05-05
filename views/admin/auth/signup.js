0a7a250f75febae6a0c5b7389d2bb4e438e144a0
NathanielDaniels <26263599+NathanielDaniels@users.noreply.github.com>
2020-05-04 23:42:07 -0500

    signup errors complete, now for signin

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -17,11 +17,15 @@ module.exports = ({ req, errors }) => {
       Your ID is: ${req.session.userId} <hr>
       <h2>Sign Up</h2>
       <form method="POST" style="width: 50%; margin: 10px;" >
-        <input name="email" placeholder="email"/>
+        <input name="email" placeholder="email"/><br>
+        ${getError(errors, "email")}
         <input name="password" placeholder="password" style="margin: 5px 0;"/>
+        <br>
+        ${getError(errors, "password")}
         <input name="passwordConfirmation" placeholder="confirm password"/>
         <br>
-        <div class="errors">></div>
+        ${getError(errors, "passwordConfirmation")}
+        <br>
         <button style="margin: 5px 0;">Sign Up</button>
       </form>
     </div>
